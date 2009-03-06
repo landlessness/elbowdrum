959d0f6c6a2af277c06a9ee7c7b9690534b51246
Brian Mulloy <brian@mulloy.us>
2009-03-05 16:01:12 -0800

    added authentication to try to fix deploy problem

--- app/models/email.rb
+++ app/models/email.rb
@@ -6,7 +6,7 @@ class Email < ActiveRecord::Base
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email
-  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
+  validates_format_of       :email,    :with => /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i, :message => Authentication.bad_email_message
 
   aasm_column :state
   aasm_initial_state :initial => :pending
