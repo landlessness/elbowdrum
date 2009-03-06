37a6902a5ba4ae1046112d553f176b9d5eb391b6
Brian Mulloy <brian@mulloy.us>
2009-03-05 16:57:37 -0800

    removing restful authentication as a submodule

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  require 'aasm'
   helper :all # include all helpers, all the time
   include AuthenticatedSystem
   before_filter :set_person_time_zone
