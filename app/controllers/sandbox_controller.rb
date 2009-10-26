b37871081d5b723a8e0931524e23fceb2aa30687
Brian Mulloy <brian@mulloy.us>
2009-10-26 16:54:27 -0700

    stop checking for session

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -1,6 +1,5 @@
 class SandboxController < ApplicationController
 # ensure_authenticated_to_facebook [:except => 'index'] 
   def index 
-    @current_facebook_user = facebook_session.user
   end
 end
