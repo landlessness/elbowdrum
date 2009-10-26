5fbf62594b1ba9ade7fddd9c4894d668e8cadef1
Brian Mulloy <brian@mulloy.us>
2009-10-26 16:37:30 -0700

    more text in test page

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -1,6 +1,7 @@
 class SandboxController < ApplicationController
 # ensure_authenticated_to_facebook [:except => 'index'] 
   def index 
+    @current_facebook_user = facebook_session.user
   end
   def user_info
     
