a8fc244795aa6bc70bbc80425c2b346c5400db3c
Brian Mulloy <brian@mulloy.us>
2009-10-26 17:14:17 -0700

    user data stuff

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -1,5 +1,8 @@
 class SandboxController < ApplicationController
-# ensure_authenticated_to_facebook [:except => 'index'] 
+  ensure_authenticated_to_facebook [:except => 'index'] 
   def index 
+  end
+  def look_at_user_data
+     @current_facebook_user = facebook_session.user
   end
 end
