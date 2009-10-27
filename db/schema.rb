c310bd3851d47c76ddb4858fcc048233e7fd90da
Brian Mulloy <brian@mulloy.us>
2009-10-26 17:05:04 -0700

    db sessions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090408140707) do
+ActiveRecord::Schema.define(:version => 20091027000137) do
 
   create_table "account_memberships", :force => true do |t|
     t.integer  "account_id"
@@ -65,6 +65,16 @@ ActiveRecord::Schema.define(:version => 20090408140707) do
   end
 
   add_index "people", ["login"], :name => "index_people_on_login", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "team_memberships", :force => true do |t|
     t.integer  "team_id"
